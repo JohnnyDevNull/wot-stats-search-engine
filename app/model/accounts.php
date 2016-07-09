59293d4eae2c08e6b36f87bde2315c36f046fa4b
Philipp John <info@jplace.de>
2016-07-09 17:38:46 +0200

    Implement new jp-wargaming-api-reader step 1 (accounts)

--- app/model/accounts.php
+++ app/model/accounts.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @package jpWot
+ * @package jpWse
  * @author Philipp John <info@jplace.de>
  * @copyright (c) 2014, Philipp John
  * @license http://opensource.org/licenses/MIT MIT see LICENSE.md
  */
-class jpWotModelAccounts extends jpWotModel
+class jpWseModelAccounts extends jpWseModel
 {
 	/**
 	 * Loads the display data from given request.
@@ -14,38 +14,40 @@ class jpWotModelAccounts extends jpWotModel
 	{
 		parent::load();
 
+		$app = jpWseApp::getInstance();
+
 		switch($this->_apiCall) {
 			case 'search':
-				$this->_data = $this->getUser (
-					'search',
+				$this->_data = $this->_gameReader->getAccountList (
 					$this->_requestData[$this->_apiCall],
-					jpWotConfig::$wotApiFields['accounts']['search']
+					jpWseConfig::$wotApiFields['accounts']['search']
 				);
 				break;
 
 			case 'detail':
 				$accountID = $this->_data['account_id'] = $this->_requestData[$this->_apiCall];
 
-				$this->_data['info'] = $this->getUser (
-					'info',
+				$this->_data['info'] = $this->_gameReader->getAccountInfo (
 					$accountID,
-					jpWotConfig::$wotApiFields['accounts']['detail']
+					jpWseConfig::$wotApiFields['accounts']['detail']
 				);
 
-				$clanID = $this->_data['info']->$accountID->clan_id;
+				$clanID = $this->_data['info']->data->$accountID->clan_id;
 
 				if(!empty($clanID)) {
-					$this->_data['info']->$accountID->clan = $this->getClan (
-						'info',
+					$this->_data['info']->data->$accountID->clan = $this->_clanReader->getClansInfo (
 						$clanID,
-						array('name', 'abbreviation')
+						array('name', 'tag')
 					);
 				}
 
-				$this->_data['vehicles'] = $this->getUser (
-					'vehicles',
-					$accountID
-				);
+				$this->_data['vehicles'] = $this->_gameReader->getAccountVehicles($accountID);
+
+				$nationsModel = $app->getModelInstance('info');
+				$nationsModel->load();
+				$info =  $nationsModel->getData();
+				$this->_data['info_vehicle_nations'] = $info->data->vehicle_nations;
+				$this->_data['info_vehicle_types'] = $info->data->vehicle_types;
 
 				$this->_loadExtendedVehiclesData();
 
@@ -64,35 +66,35 @@ class jpWotModelAccounts extends jpWotModel
 	{
 		$accountID = $this->_data['account_id'];
 
-		if(empty($this->_data['vehicles']->$accountID)) {
+		if(empty($this->_data['vehicles']->data->$accountID)) {
 			return;
 		}
 
-		$vehicles = $this->_data['vehicles']->$accountID;
+		$vehicles = $this->_data['vehicles']->data->$accountID;
 		$this->_data['tankinfo'] = array();
 		$tankIdArray = array();
 
 		foreach($vehicles as $vehicle) {
 			/*
 			 * Fetch the tank_id's to an array, that we can get the tankinfo in
-			 * one bigger api call instead of many single api calls. This will
-			 * be very much faster.
+			 * one bigger api call instead of many single api calls.
 			 */
 			$tankIdArray[] = $vehicle->tank_id;
 		}
 
-		if(!empty($tankIdArray)) {
-			$this->_data['tankinfo'] = $this->getWiki (
-				'tankinfo',
-				implode(',', $tankIdArray),
-				jpWotConfig::$wotApiFields['wiki']['tankinfo']
-			);
+		if(empty($tankIdArray)) {
+			return;
 		}
 
-		foreach($this->_data['tankinfo'] as $tankInfo) {
+		$this->_data['tankinfo'] = $this->_gameReader->getEncyclopediaVehicles (
+			implode(',', $tankIdArray),
+			jpWseConfig::$wotApiFields['wiki']['tankinfo']
+		);
+
+		foreach($this->_data['tankinfo']->data as $tankInfo) {
 			if(!isset($this->_data['vehicle_types'][$tankInfo->type])) {
 				$this->_data['vehicle_types'][$tankInfo->type] = array(
-					'type_i18n' => $tankInfo->type_i18n,
+					'type_i18n' => $this->_data['info_vehicle_types']->{$tankInfo->type},
 					'wins' => 0,
 					'battles' => 0,
 				);
@@ -100,7 +102,7 @@ class jpWotModelAccounts extends jpWotModel
 
 			if(!isset($this->_data['nations'][$tankInfo->nation])) {
 				$this->_data['nations'][$tankInfo->nation] = array(
-					'nation_i18n' => $tankInfo->nation_i18n,
+					'nation_i18n' => $this->_data['info_vehicle_nations']->{$tankInfo->nation},
 					'wins' => 0,
 					'battles' => 0,
 				);
@@ -109,8 +111,8 @@ class jpWotModelAccounts extends jpWotModel
 
 		foreach($vehicles as $vehicle)
 		{
-			$type = $this->_data['tankinfo']->{$vehicle->tank_id}->type;
-			$nation = $this->_data['tankinfo']->{$vehicle->tank_id}->nation;
+			$type = $this->_data['tankinfo']->data->{$vehicle->tank_id}->type;
+			$nation = $this->_data['tankinfo']->data->{$vehicle->tank_id}->nation;
 
 			$this->_data['vehicle_types'][$type]['wins'] += $vehicle->statistics->wins;
 			$this->_data['vehicle_types'][$type]['battles'] += $vehicle->statistics->battles;
